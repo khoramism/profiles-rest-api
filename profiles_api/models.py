3797d0d5191f91af0cc2411ee0b56e75ddee7f85
Alireza Khorami <khoramism@gmail.com>
2021-07-25 13:37:35 +0430

    Enabled Django admin for User pRofile model and ..

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,58 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-
 class UserProfileManager(BaseUserManager):
-    ''' Manager for user profiles.'''
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        # A none password won't work so bascially if you don't have a password you've got no user 
-
+        """Create a new user profile"""
         if not email:
-            raise ValueError('شما باید یک ایمیل داشته باشید! :/ ')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
+
         user.set_password(password)
         user.save(using=self._db)
 
-        return user 
+        return user
 
     def create_superuser(self, email, name, password):
-        '''Create and Save a new Superuser with given detailes man.'''
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
-        user.is_superuser = True 
-        user.is_staff = True 
-        user.save(using = self._db)
 
-        return user 
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-
-
-
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    '''Database Model for users in the system.'''
+    """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name  = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-
-    objects  = UserProfileManager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
-    
+    REQUIRED_FIELDS = ['name']
+
     def get_full_name(self):
-        ''' Retrieve full name of the user '''
+        """Retrieve full name for user"""
         return self.name
-        
+
     def get_short_name(self):
-        '''Retrieve short name of the user '''
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        '''Return String representation of user '''
-        return self.email 
+        """Return string representation of user"""
+        return self.email
