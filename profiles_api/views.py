02e27b56847eb1dad31cba29f817688f2e304998
Alireza Khorami <khoramism@gmail.com>
2021-07-27 18:02:04 +0430

    Added viewset to the api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status 
+from rest_framework import status, viewsets 
+
 # Internals
 
 from profiles_api import serializers
@@ -56,14 +57,68 @@ class HelloApiView(APIView):
 
 
 
-
-
 '''The main difference between the PUT and PATCH method
  is that the PUT method uses the request URI to supply a modified version of the requested resource
   which replaces the original version of the resource, 
 whereas the PATCH method supplies a set of instructions to modify the resource.
 Put Entirely 
 Patch partially
-
 '''
     
+
+
+class HelloViewSet(viewsets.ViewSet):
+    '''Test Api view set!'''
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        '''Return a Hello message'''
+        a_viewset = [
+            'User actions (list, create, retrieve, update, partial_upodate)',
+            'Automatically maps to Urls using routers.',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message':'Hellow', 'a_viewset':a_viewset})
+
+    def create(self, request):
+        '''Create a new Hello message!'''
+
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status =status.HTTP_400_BAD_REQUEST)
+
+
+    def retrieve(self, request, pk=None):
+        '''Handel getting an object by its id(pk)'''
+        return Response({'http_method':'GET'})
+    
+    def update(self, request,pk=None):
+        '''Handle updating an object'''
+        return Response({'http_method':'PUT'})
+    
+
+    def partial_update(self, request,pk=None):
+        '''Handle updating part of an object'''
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request,pk=None):
+        '''Handel deleting an object by its id(pk)'''
+        return Response({'http_method':'DELETE'})
+
+
+
+
+
+
+
+
+
+
+
